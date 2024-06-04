6d8c857120f1731bdadd5000261c368e305c38fd
Radouane GAROIAZ <radouane.garoiaz@expaceo.com>
2024-06-04 17:39:38 +0200

    refactor: Remove commented out code in Newsletter component

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,11 +1,11 @@
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
+// import { Button } from "./ui/button";
+// import { Input } from "./ui/input";
 
 export const Newsletter = () => {
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log("Subscribed!");
-  };
+  // const handleSubmit = (e: any) => {
+  //   e.preventDefault();
+  //   console.log("Subscribed!");
+  // };
 
   return (
     <section id="newsletter">
@@ -13,16 +13,20 @@ export const Newsletter = () => {
 
       <div className="container py-24 sm:py-32">
         <h3 className="text-center text-4xl md:text-5xl font-bold">
-          Join Our Daily{" "}
+         
           <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-            Newsletter
+          5 Eye{" "}
+          </span>
+          By the community, for the{" "}
+          <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+          community.
           </span>
         </h3>
         <p className="text-xl text-muted-foreground text-center mt-4 mb-8">
-          Lorem ipsum dolor sit amet consectetur.
+          Crafted with love ❤️
         </p>
 
-        <form
+        {/* <form
           className="flex flex-col w-full md:flex-row md:w-6/12 lg:w-4/12 mx-auto gap-4 md:gap-2"
           onSubmit={handleSubmit}
         >
@@ -32,7 +36,7 @@ export const Newsletter = () => {
             aria-label="email"
           />
           <Button>Subscribe</Button>
-        </form>
+        </form> */}
       </div>
 
       <hr className="w-11/12 mx-auto" />
