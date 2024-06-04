84cc19dcfb071a34265e7714492369f548cef05a
Dudu49cls <garoiaz.radouane@gmail.com>
2024-06-05 00:03:49 +0200

    Update package-lock.json and package.json with @vercel/analytics dependency

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
